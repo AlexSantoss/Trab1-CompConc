554b258550b4faf96f7a22b38679e8f14c39c99b
Cappelli <elizeu.cappelli@gmail.com>
2018-09-10 19:59:38 -0300

    Update barbeiro.c

--- barbeiro.c
+++ barbeiro.c
@@ -99,3 +99,23 @@ void *barber(void *nada) {
         }
     }
 }
+
+/* 
+O problema é baseado numa barbearia hipotética com somente um barbeiro. O barbearia tem somente uma cadeira na sala de corte de cabelo e uma sala de espera com um certo número de cadeiras.
+Ao terminar de fazer o corte de cliente, ele libera esse cliente e vai pra sala de espera ver se tem algum outro cliente esperando.
+Se tiver alguém esperando, ele leva esse cliente para a sala e corta o cabelo dele.
+Se não tiver ninguém, ele volta para a cadeira da sala de corte de cabelo e dorme nela.
+
+Ao chegar, cada cliente verifica o que o barbeiro está fazendo. 
+Se ele estiver dormindo, o cliente o acorda e senta na cadeira da sala de corte.
+Se ele estiver cortando o cabelo de outro cliente, o cliente que acabou de chegar vá para a sala de espera.
+Na sala de espera, ele verifica se tem algum lugar vago, caso tenha ele senta nesse lugar e espera a sua vez chegar. Caso contrário, ele sai da barbearia.
+*/
+
+/*
+Um dos problemas dessa solução é que existe um certo atraso para as ações do cliente e do barbeiro acontecerem. 
+Por exemplo, se um cliente chega na barbearia enquanto o barbeiro está cortando o cabelo, ele vai levar um tempo de verificar que o barbeiro está ocupado e de chegar na sala de espera, 
+nesse meio tempo, o barbeiro pode terminar de fazer o corte e verificar que não há ninguém sala, assim irá voltar a sua sala e dormir.
+Dessa forma o cliente estará, agora, esperando o barbeiro ficar disponível e o barbeiro estará dormindo, pois não tem nenhum cliente.
+Um outro problema que pode ocorrer é se dois clientes chegarem ao mesmo tempo e só tiverem uma vaga disponível na sala de espera, eles vão tentar ocupar a mesma cadeira.
+*/
